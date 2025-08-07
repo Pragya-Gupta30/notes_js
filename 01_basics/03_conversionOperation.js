a3db265e4c33e793621b065e6832d334b60f80b4
Pragya-Gupta30 <151020329+Pragya-Gupta30@users.noreply.github.com>
2025-08-07 06:17:33 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,15 +1,15 @@
-let score = "hitesh"
+let score = "33abc"
 
 //console.log(typeof score);
-//console.log(typeof(score));
+//console.log(typeof(score)); dono same hi hai bas isme as a method use kiya hai
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+//console.log(typeof valueInNumber); number convert to ho rha hai lekin number hai nhi mtlb type number hai but value number nhi hai...
+//console.log(valueInNumber); NaN
 
-
+//score=null ye kiya toh 0 ho jayega number karne pe
 // "33" => 33
-// "33abc" => NaN
+// "33abc" => NaN not a number
 //  true => 1; false => 0
 
 let isLoggedIn = "hitesh"
@@ -24,14 +24,14 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(stringNumber); 
+// console.log(typeof stringNumber); string type ho jayega
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue); negative value de diya uska bas
 
 // console.log(2+2);
 // console.log(2-2);
@@ -43,26 +43,26 @@ let negValue = -value
 let str1 = "hello"
 let str2 = " hitesh"
 
-let str3 = str1 + str2
+let str3 = str1 + str2 //simple join kar diya do strings ko
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); //yaha pahle string tha toh dono ko string kar diya jo baad mein tha
+console.log(1 + 2 + "2"); //yaha pahle operation hua uske baad string banaya hai
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); //iska value 1 hai
+// console.log(+""); //idhar bhi 1 hoga
 
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
-++gameCounter;
+++gameCounter; //prefix 
 console.log(gameCounter);
-
+//gameCounter++; // postfix
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion

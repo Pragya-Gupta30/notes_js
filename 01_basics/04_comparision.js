551d6f941036167673fa1bff8263ba14ac5285e2
Pragya-Gupta30 <151020329+Pragya-Gupta30@users.noreply.github.com>
2025-08-07 06:28:48 +0000

    comaprision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,5 +1,5 @@
 // console.log(2 > 1);
-// console.log(2 >= 1);
+// console.log(2 >= 1); //true false mein value aayegi
 // console.log(2 < 1);
 // console.log(2 == 1);
 // console.log(2 != 1);
@@ -10,7 +10,9 @@
 
 console.log(null > 0);
 console.log(null == 0);
-console.log(null >= 0);
+console.log(null >= 0); //sab kuch false tha lekin ye true hai
+//reason: equality check == and comparision ><>=<= works differently.
+//Comparisions convert null to a number, treating it as a 0.
 
 console.log(undefined == 0);
 console.log(undefined > 0);
@@ -18,4 +20,4 @@ console.log(undefined < 0);
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2); //checks datatype also

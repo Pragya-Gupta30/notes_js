b50e9c81d540d562ac76587e5fd5c3df824d2761
Pragya-Gupta30 <151020329+Pragya-Gupta30@users.noreply.github.com>
2025-08-11 08:40:53 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,47 +1,55 @@
-const user = {
+ const user = {
     username: "hitesh",
     price: 999,
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(this); //pura current context deta hai
     }
 
 }
 
 // user.welcomeMessage()
 // user.username = "sam"
-// user.welcomeMessage()
+// user.welcomeMessage() 
 
-// console.log(this);
+console.log(this); //yaha pe ye empty hoga {}
+
+
+// +++++++important hai yeh wala++++++++
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); //ye nhi chalega kyuki idhar koi object nhi hai ye direct variable hai...iska answer undefined hai
 // }
 
-// chai()
+//chai() 
 
 // const chai = function () {
 //     let username = "hitesh"
 //     console.log(this.username);
-// }
+// } //ye bhi undefined rahega
+
+
+//Arrow function
 
 const chai =  () => {
     let username = "hitesh"
     console.log(this);
 }
+chai()
+
+const adTwo = (num1, num2) => {
+    return num1 + num2
+}
 
 
-// chai()
+//***********curly braces mein wrap kiya toh return likhna padega aur agar parenthesis mein kiya toh jarurat nhi hai********** */
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+const add_Two = (num1, num2) =>  num1 + num2 
+//implicit return braces hata sakte hain
 
-// const addTwo = (num1, num2) =>  num1 + num2
-
-// const addTwo = (num1, num2) => ( num1 + num2 )
+const add__Two = (num1, num2) => ( num1 + num2 )
 
 const addTwo = (num1, num2) => ({username: "hitesh"})
 
